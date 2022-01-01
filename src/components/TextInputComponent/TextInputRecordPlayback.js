0b6eb6cbd4e1941134f7ac89afa88827cc665b6e
Julia Snegova <ssnegss@gmail.com>
2022-01-01 20:06:38 +0300

    Buttons in windowComponent at playback/record page are added

--- src/components/TextInputComponent/TextInputRecordPlayback.js
+++ src/components/TextInputComponent/TextInputRecordPlayback.js
@@ -1,36 +1,46 @@
-import React from 'react';
-import { View, TextInput } from 'react-native';
+import React from "react";
+import { View, TextInput, Button } from "react-native";
 
 const WindowTextInput = (props) => {
-  return (
-    <TextInput
-      {...props} // Inherit any props passed to it; e.g., multiline, numberOfLines below
-      editable
-    />
-  );
-}
+    return (
+        <TextInput
+            {...props} // Inherit any props passed to it; e.g., multiline, numberOfLines below
+            editable
+        />
+    );
+};
 
 const WindowTextInputMultiline = () => {
-  const [value, onChangeText] = React.useState('');
+    const [value, onChangeText] = React.useState("");
 
-  // If you type something in the text box that is a color, the background will change to that
-  // color.
-  return (
-    <View
-      style={{
-        borderBottomColor: '#000000',
-        borderBottomWidth: 1,
-        width: '90%'
-      }}>
-      <WindowTextInput
-        multiline
-        numberOfLines={4}
-        onChangeText={text => onChangeText(text)}
-        value={value}
-        style={{padding: 10}}
-      />
-    </View>
-  );
-}
+    // If you type something in the text box that is a color, the background will change to that
+    // color.
+    return (
+        <>
+            <View
+                style={{
+                    borderBottomColor: "#000000",
+                    borderBottomWidth: 1,
+                    width: "90%",
+                }}
+            >
+                <WindowTextInput
+                    multiline
+                    numberOfLines={4}
+                    onChangeText={(text) => onChangeText(text)}
+                    value={value}
+                    style={{ padding: 10 }}
+                />
+            </View>
+            <View style={{ flexDirection: "row" }}>
+                    <Button title="1"></Button>
+                    <Button title="1"></Button>
+                    <Button title="1"></Button>
+                    <Button title="1"></Button>
+
+            </View>
+        </>
+    );
+};
 
 export default WindowTextInputMultiline;
